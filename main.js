218a773831953db2cda1d62563986c96f7d459e3
CodeManMalck <51975207+CodeManMalck@users.noreply.github.com>
2025-11-24 18:40:52 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-// Write your code below
+function findMinLength(word1, word2) {
+  var shortest;
+  if (word1.length < word2.length) {
+    shortest = word1.length;
+  } else {
+    shortest = word2.length;
+  }
+
+  return shortest;
+}

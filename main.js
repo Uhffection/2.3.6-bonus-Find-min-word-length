c9957182ea13b088b1e850b8292f11b638d2124e
CodeManMalck <51975207+CodeManMalck@users.noreply.github.com>
2025-11-26 20:02:20 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,1 @@
-function findMinLength(word1, word2) {
-  var shortest;
-  if (word1.length < word2.length) {
-    shortest = word1.length;
-  } else {
-    shortest = word2.length;
-  }
 
-  return shortest;
-}

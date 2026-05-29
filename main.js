cd84793c2d95ed545e6e0e324d0c95e205dd4fa3
Uhffection <142746146+Uhffection@users.noreply.github.com>
2026-05-29 22:23:32 +0000

    Create function findMinLength to find length of shortest word out of 2 arguments

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
+function findMinLength(word1,word2) {
+    if (word1.length < word2.length){
+        return word1.length
+}   if (word1.length > word2.length) {
+        return word2.length
+} 
+    }
 
+    var output = findMinLength("abracadabra","hippopotamus")
+
+    console.log(output)
